b48cb684a418b932ab4bd1349dd610445f129ff9
Aziza Baccouri <azizabaccouri@gmail.com>
2023-06-09 10:22:02 +0200

    DOM drill

--- 7.DOM-drill/script.js
+++ 7.DOM-drill/script.js
@@ -1,13 +1,14 @@
 let ul = document.querySelector("ul");
-let ulChildNodes = ul.childNodes;
-console.log(ulChildNodes);
+let ulChildNodes = ul.children;
+console.log(ulChildNodes[1].nodeType);
 
 for(let i=0;i<ulChildNodes.length;i++){
-    console.log(ulChildNodes[i].nodeType);
+    console.log("loop for"+ulChildNodes[i].nodeType);
     if(ulChildNodes[i].nodeType===1){
+        console.log("aaaaaaaaaaaa")
         console.log("index : " ,i);
         if((ulChildNodes[i].textContent=="Fast and Furious") && (i>0)){
-            console.log(ulChildNodes[i].textContent+" at index "+i);
+
             //Since Fast and Furious is the most important franchise ever, add a class .important to the element
             ulChildNodes[i].className ="important";
             ul.prepend(ulChildNodes[i]);
@@ -30,19 +31,21 @@ for(let i=0;i<lichildren.length;i++){
    
     
 });}
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 1; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
 
 
-  ///////// NOT FINISHED ///////////
+
+ // sorted in a random order, however Fast and Furious should remain the first element (
 document.body.addEventListener("keyup",  (e)=> {
     if(e.code =="KeyR"){
-        shuffleArray(lichildren);
+        console.log(ul.children);
+        for (var i = ul.children.length; i > 0; i--) {
+            ul.appendChild(ul.children[Math.random() * i | 0]);
+            let importantLi = ul.querySelector(".important");
+
+            if (importantLi) {
+              ul.prepend(importantLi);
+            }
+        }
 
     }
     
